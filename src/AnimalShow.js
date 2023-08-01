7fd17b06cc503bf3c470dfaa85ace186f66d95a9
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-07-31 21:04:58 -0300

    Add logic to show animals

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,3 +1,34 @@
-export default function AnimalShow () {
-    return <div>Cow!</div>
+import bird from './source/bird.svg'
+import cat from './source/cat.svg'
+import cow from './source/cow.svg'
+import dog from './source/dog.svg'
+import gator from './source/gator.svg'
+import heart from './source/heart.svg'
+import horse from './source/horse.svg'
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+};
+
+export default function AnimalShow ({ type }) {
+    const [clicks, setClicks] = useState();	
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+
+    return (
+        <div onClick={handleClick}>
+            <img src={svgMap[type]} alt="animal" />
+            <img src={heart}
+            alt="heart"
+            style={{ width: 10 + 10 * clicks, height: 10 + 10 * clicks, position: 'absolute', top: 10, right: 10 }}
+             />
+        </div>
+    )
 }
--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import AnimalShow from "./AnimalShow";
 import { useState } from 'react'
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 export default function App () {
-    let [count, setCount] = useState(0);
+    let [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    }
+
+    let renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
 
     return (
         <div>
-            <button onClick={() => {
-                setCount(++count)
-            }}>
-                Add animal
-            </button>
-            <div>Number of animals: {count}</div>
+            <button onClick={handleClick}>Add animal</button>
+            <div>{renderedAnimals}</div>
         </div>
     );
 }
